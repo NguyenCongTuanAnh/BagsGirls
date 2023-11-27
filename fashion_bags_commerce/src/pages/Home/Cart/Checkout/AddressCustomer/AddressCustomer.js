d32f872d1ad3189ddd6dc2295f969ffe1d18d8e6
HUYNHPHUNG\Huynh <huynhphung2107@gmail.com>
2023-11-27 23:16:22 +0700

    update ban hang online

--- fashion_bags_commerce/src/pages/Home/Cart/Checkout/AddressCustomer/AddressCustomer.js
+++ fashion_bags_commerce/src/pages/Home/Cart/Checkout/AddressCustomer/AddressCustomer.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './styles.scss';
 import { Link } from 'react-router-dom';
+import billsAPI from '~/api/BillApi';
+import dayjs from 'dayjs';
+import { generateCustomCode } from '~/Utilities/GenerateCustomCode';
 
 const AddressVietnam = () => {
   const host = 'https://provinces.open-api.vn/api/';
@@ -14,6 +17,34 @@ const AddressVietnam = () => {
   const [fullName, setFullName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [address, setAddress] = useState('');
+
+  const [billCreateDate, setBillCreateDate] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const currentDateTime = dayjs().format('YYYY-MM-DD HH:mm:ss');
+    setBillCreateDate(currentDateTime);
+    const fullAddress = `${address} | ${selectedWard} | ${selectedDistrict} | ${selectedProvince}`;
+
+    try {
+      const billData = {
+        receiverName: fullName,
+        orderPhone: phoneNumber,
+        shippingAddress: fullAddress,
+        billCreateDate: currentDateTime, // Get current date in ISO format
+        billStatus: 4,
+        billCode: generateCustomCode('Bill', 4), // Generate custom bill code
+      };
+
+      // Send the Bill object to the API using billsAPI.add()
+      const response = await billsAPI.add(billData);
+      console.log('Successful information submission, Bill Code:', response.data.billCode);
+      console.log('Successful information submission:', response.data);
+    } catch (error) {
+      console.error('Error submitting information:', error);
+    }
+  };
 
   useEffect(() => {
     axios
@@ -62,20 +93,30 @@ const AddressVietnam = () => {
     setSelectedWard(wardCode);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const address = `${selectedProvince} | ${selectedDistrict} | ${selectedWard}`;
-    // Gửi thông tin người nhận hàng và địa chỉ đi đâu đó, ví dụ:
-    console.log(`Người nhận: ${fullName}, Số điện thoại: ${phoneNumber}, Địa chỉ: ${address}`);
-    // Hoặc có thể gửi thông tin này đến một API khác
+  const getAddressName = (code, type) => {
+    let name = '';
+    switch (type) {
+      case 'ward':
+        name = wards.find((ward) => ward.code === code)?.name || '';
+        break;
+      case 'district':
+        name = districts.find((district) => district.code === code)?.name || '';
+        break;
+      case 'province':
+        name = provinces.find((province) => province.code === code)?.name || '';
+        break;
+      default:
+        break;
+    }
+    return name;
   };
 
   return (
     <div className="form-container">
       <form onSubmit={handleSubmit}>
-      <div className="titleNhanHang">
-  <h1>Thông tin người đặt hàng</h1>
-</div>
+        <div className="titleNhanHang">
+          <h1>Thông tin người đặt hàng</h1>
+        </div>
         <p>
           Bạn đã có tài khoản?{' '}
           <span>
@@ -83,14 +124,16 @@ const AddressVietnam = () => {
           </span>
         </p>
         <br></br>
-       <input className="inputLabel"
+        <input
+          className="inputLabel"
           type="text"
           value={fullName}
           onChange={(e) => setFullName(e.target.value)}
           placeholder="Họ và tên"
           required
         />
-       <input className="inputLabel"
+        <input
+          className="inputLabel"
           type="tel"
           value={phoneNumber}
           onChange={(e) => setPhoneNumber(e.target.value)}
@@ -127,7 +170,8 @@ const AddressVietnam = () => {
             </option>
           ))}
         </select>
-       <input className="inputLabel"
+        <input
+          className="inputLabel"
           type="text"
           value={address}
           onChange={(e) => setAddress(e.target.value)}
