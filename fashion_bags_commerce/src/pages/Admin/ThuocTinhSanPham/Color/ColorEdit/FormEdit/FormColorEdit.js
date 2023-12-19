f8a5099e344a92147ef45831dae21dd4339f88e0
HUYNHPHUNG\Huynh <huynhphung2107@gmail.com>
2023-12-20 01:02:44 +0700

    no message

--- fashion_bags_commerce/src/pages/Admin/ThuocTinhSanPham/Color/ColorEdit/FormEdit/FormColorEdit.js
+++ fashion_bags_commerce/src/pages/Admin/ThuocTinhSanPham/Color/ColorEdit/FormEdit/FormColorEdit.js
@@ -5,170 +5,143 @@ import colorAPI from '~/api/propertitesBalo/colorAPI';
 
 function FormcolorEdit(props) {
   const [open, setOpen] = useState(false);
-  const [error, setError] = useState(false);
-  const [data, setData] = useState(props.color);
-  const [stringStatus, setStringStatus] = useState('');
-  const [reloadTable, setReloadTable] = useState(false);
-  const [form] = Form.useForm();
+    const [error, setError] = useState(false);
+    const [form] = Form.useForm();
 
-  const validateTypeName = (rule, value, callback) => {
-    if (value && !/^[a-zA-ZÀ-ỹ]+(\s[a-zA-ZÀ-ỹ]+)*$/.test(value)) {
-      callback('Tên kiểu không hợp lệ!');
-    } else {
-      callback();
-    }
-  };
+    const showComponent = () => {
+        setOpen(true);
+        form.resetFields();
+    };
 
-  const showComponent = () => {
-    setOpen(true);
-    if (data.colorStatus === 1) {
-      setStringStatus('Hoạt động');
-    } else if (data.colorStatus === -1) {
-      setStringStatus('Ngừng hoạt động');
-    } else {
-      setStringStatus('Không hoạt động');
-    }
-  };
+    const closeComponent = () => {
+        setOpen(false);
 
-  const closeComponent = () => {
-    setOpen(false);
-    form.getFieldValue();
-  };
-
-  const updateData = (event) => {
-    const { name, value } = event.target;
-    setData({ ...data, [name]: value });
-  };
-  const updateStatus = (value) => {
-    setData({ ...data, colorStatus: value });
-  };
-
-  const updateFunction = async (colorId, values) => {
-    setError(false);
-    let update = { ...values };
-    console.log(colorId);
-    console.log(update);
-    if (!error) {
-      try {
-        await colorAPI.update(colorId, update);
-        notification.success({
-          message: 'Cập nhật thành công',
-          description: 'Dữ liệu đã được cập nhật thành công',
-          duration: 2,
+    };
+    const validatecolorName = async (rule, value) => {
+        return new Promise((resolve, reject) => {
+            if (value && !/^[a-zA-ZÀ-ỹ]+(\s[a-zA-ZÀ-ỹ]+)*$/.test(value)) {
+                reject('Tên màu sắc không hợp lệ!');
+            } else {
+                resolve();
+            }
         });
-        closeComponent();
-        props.reload();
-      } catch (error) {
-        console.log(error);
-        setError(true);
-        notification.error({
-          message: 'Cập nhật thất bại',
-          description: 'Dữ liệu không được cập nhật',
-          duration: 2,
-        });
-      }
-    }
-  };
+    };
 
-  return (
-    <Fragment>
-      <div style={{ color: 'red' }}>
-        <Button
-          type="default"
-          style={{ border: '1px blue solid', color: 'blue' }}
-          onClick={showComponent}
-          icon={<EditOutlined />}
-        >
-          Sửa
-        </Button>
-        <Drawer
-          title={'Edit - ' + data.colorCode}
-          width={400}
-          onClose={closeComponent}
-          open={open}
-          style={{
-            paddingBottom: 80,
-          }}
-          extra={
-            <Space>
-              <Button onClick={closeComponent}>Thoát</Button>
-              <Button onClick={() => updateFunction(data.colorId, data)} type="primary" className="btn btn-warning">
-                Lưu
-              </Button>
-            </Space>
-          }
-        >
-          <Form
-            layout="vertical"
-            hideRequiredMark
-            initialValues={data}
-            form={form}
-          >
-            <Row gutter={16}>
-              <Col span={24}>
-                <Form.Item
-                  name="colorCode"
-                  label="Mã"
-                  rules={[
-                    {
-                      required: true,
-                      message: 'Vui lòng điền Mã Color',
-                    },
-                  ]}
+    const updateFunction = async (values) => {
+
+        setError(false);
+        let update = {
+           colorId: props.color.colorId,
+           colorCode: props.color.colorCode,
+           colorName: values.colorName,
+           colorStatus: values.colorStatus,
+        };
+        if (!error) {
+            try {
+                await colorAPI.update(props.color.colorId, update);
+                notification.success({
+                    message: 'Cập nhật thành công',
+                    description: 'Dữ liệu đã được cập nhật thành công',
+                    duration: 2,
+                });
+                props.reload();
+                closeComponent();
+            } catch (error) {
+                console.log(error);
+                setError(true);
+                notification.error({
+                    message: 'Cập nhật thất bại',
+                    description: 'Dữ liệu không được cập nhật',
+                    duration: 2,
+                });
+            }
+        }
+    }
+
+    return (
+        <Fragment>
+            <div style={{ color: 'red' }}>
+                <Button
+                   color="default"
+                    style={{ border: '1px blue solid', color: 'blue' }}
+                    onClick={showComponent}
+                    icon={<EditOutlined />}
                 >
-                  <Input placeholder="Vui lòng điền Mã Color" disabled />
-                </Form.Item>
-              </Col>
-            </Row>
-            <Row gutter={16}>
-              <Col span={24}>
-                <Form.Item
-                  name="colorName"
-                  label="Tên"
-                  rules={[
-                    {
-                      required: true,
-                      message: 'Vui lòng điền tên màu sắc!',
-                    },
-                    {
-                      validator: validateTypeName,
-                    },
-                  ]}
+                    Sửa
+                </Button>
+                <Drawer
+                    title={'Chỉnh sửa màu sắc có mã: ' + props.color.colorCode}
+                    width={400}
+                    onClose={closeComponent}
+                    open={open}
+                    style={{
+                        paddingBottom: 80,
+                    }}
+                    footer={
+                        null
+                    }
                 >
-                  <Input
-                    name="colorName"
-                    value={data.colorName}
-                    onChange={updateData}
-                    placeholder="Vui lòng điền tên color"
-                  />
-                </Form.Item>
-              </Col>
-            </Row>
-            <Row gutter={16}>
-              <Col span={24}>
-                <Form.Item
-                  name="colorStatus"
-                  label="Trạng Thái"
-                  rules={[
-                    {
-                      required: true,
-                      message: 'Vui lòng chọn Trạng Thái',
-                    },
-                  ]}
-                >
-                  <Select onChange={updateStatus} defaultValue={stringStatus} placeholder="Vui lòng chọn Trạng Thái">
-                    <Select.Option value="1">Hoạt động</Select.Option>
-                    <Select.Option value="0">Không hoạt động</Select.Option>
-                    <Select.Option value="-1">Ngừng hoạt động</Select.Option>
-                  </Select>
-                </Form.Item>
-              </Col>
-            </Row>
-          </Form>
-        </Drawer>
-      </div>
-    </Fragment>
-  );
+                    <Form layout="vertical"
+                        initialValues={props.type}
+                        onFinish={updateFunction}
+                        form={form}>
+
+                        <Row gutter={16}>
+                            <Col span={24}>
+                                <Form.Item
+                                    name="colorName"
+                                    label="Tên màu sắc"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: 'Vui lòng điền tên màu sắc!',
+                                        },
+                                        {
+                                            validator: validatecolorName,
+                                        },
+                                    ]}
+                                >
+                                    <Input
+                                        placeholder="Vui lòng điền tên màu sắc"
+                                    />
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                        <Row gutter={16}>
+                            <Col span={24}>
+                                <Form.Item
+                                    label="Trạng thái"
+                                    name="colorStatus"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: 'Vui lòng chọn trạng thái!',
+                                        },
+                                    ]}
+                                >
+                                    <Select
+                                        name="colorStatus"
+                                        placeholder="Vui lòng chọn trạng thái"
+                                    >
+                                        <Select.Option value={1}>Hoạt động</Select.Option>
+                                        <Select.Option value={0}>Ngừng hoạt động</Select.Option>
+                                    </Select>
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Space style={{ textAlign: 'right' }}>
+                                <Button onClick={closeComponent}>Thoát</Button>
+                                <Button type="primary" htmlType="submit">
+                                    Lưu
+                                </Button>
+                            </Space>
+                        </Row>
+                    </Form>
+                </Drawer>
+            </div>
+        </Fragment>
+    );
 }
 
 export default FormcolorEdit;
